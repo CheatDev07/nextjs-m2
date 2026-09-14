90075cdae9da5c84e94d9e5fc3d72ce3348be277
SokcheatSrorng <sokcheatsrorng@gmail.com>
2026-09-14 10:39:40 +0700

    Factor: Deploy v6

--- src/app/product/[...id]/page.tsx
+++ src/app/product/[...id]/page.tsx
@@ -1,35 +1,35 @@
 import { ProductDetailComponent } from "@/components/products/ProductDetailComponent";
 
-// // implement generate metadata
-// import type { Metadata, ResolvingMetadata } from 'next'
+// implement generate metadata
+import type { Metadata, ResolvingMetadata } from 'next'
  
-// type Props = {
-//   params: Promise<{ id: number }>
-//   searchParams: Promise<{ [key: string]: string | string[] | undefined }>
-// }
+type Props = {
+  params: Promise<{ id: number }>
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}
  
-// export async function generateMetadata(
-//   { params, searchParams }: Props,
-//   parent: ResolvingMetadata
-// ): Promise<Metadata> {
-//   // read route params
-//   const { id } = await params
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const { id } = await params
  
-//   // fetch data
-//   const product = await fetch(`${process.env.FAKESTORE_API}/products/${id}`).then((res) => res.json())
+  // fetch data
+  const product = await fetch(`${process.env.FAKESTORE_API}/products/${id}`).then((res) => res.json())
  
-//   // optionally access and extend (rather than replace) parent metadata
-//   // const previousImages = (await parent).openGraph?.images || []
+  // optionally access and extend (rather than replace) parent metadata
+  // const previousImages = (await parent).openGraph?.images || []
  
-//   return {
-//     title: product.title,
-//     openGraph: {
-//       title: product.title,
-//       description: product.description,
-//       images: [`${product.image}`],
-//     },
-//   }
-// }
+  return {
+    title: product.title,
+    openGraph: {
+      title: product.title,
+      description: product.description,
+      images: [`${product.image}`],
+    },
+  }
+}
 
 export default async function ProductDetailPage({
   params
